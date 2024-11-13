899638dcad6ed5b919b2a7a862ab674fc7d3e114
yodemisj <yodemisj@gmail.com>
2024-11-13 10:24:22 -0300

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import route from "./routes/route";
+
+const PORT = process.env.PORT || 3000; // Se a porta não estiver definida nas váriaveis de ambiente usa a porta 3000
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(route);
+
+app.listen(PORT, () => { // Inicia o servidor
+    console.log(`Server running on http://localhost:${PORT}`);
+});
